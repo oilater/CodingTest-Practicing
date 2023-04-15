36d8da90d4564f521c4d52d1f69155618f040b7b
김성현 <oilater@naver.com>
2023-04-16 04:19:29 +0900

    commit

---
+++ 2023-04-16/이진수더하기/solution1.js
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120885?language=javascript
+
+function solution(bin1, bin2) {
+  var answer = "";
+  return answer;
+}
+
+console.log(solution("10", "11")); // "101"
+console.log(solution("1001", "1111")); // "11000"
--- 2023-04-16/한번만등장한문자/solution1.js
+++ 2023-04-16/한번만등장한문자/solution1.js
@@ -1,7 +1,31 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120896?language=javascript
 
 function solution(s) {
-  var answer = "";
+  let temp = "";
+  let answer = "";
+  const eng = "abcdefghijklmnopqrstuvwxyz";
+  let count = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    count = 0;
+    for (let j = 0; j < s.length; j++) {
+      if (s[i] === s[j]) {
+        count++;
+      }
+    }
+
+    if (count === 1) {
+      temp += s[i];
+    }
+  }
+
+  for (let i = 0; i < eng.length; i++) {
+    for (let j = 0; j < temp.length; j++) {
+      if (eng[i] === temp[j]) {
+        answer += temp[j];
+      }
+    }
+  }
 
   return answer;
 }
