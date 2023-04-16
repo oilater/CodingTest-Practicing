935ab4d25ec92fa6d2e23c4f7e7e1c4ff8d094ed
김성현 <oilater@naver.com>
2023-04-16 14:23:02 +0900

    nangman

--- 2023-04-16/이진수더하기/solution1.js
+++ 2023-04-16/이진수더하기/solution1.js
@@ -1,8 +1,47 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120885?language=javascript
 
 function solution(bin1, bin2) {
-  var answer = "";
-  return answer;
+  const answer = "";
+  let n1 = 0;
+  let n2 = 0;
+  let num = 0;
+  let maxNum = 0;
+  let count = 0;
+  let arr = [];
+
+  for (let i = 0; i < bin1.length; i++) {
+    const j = bin1.length - i - 1;
+    n1 += bin1[j] * 2 ** i;
+  }
+
+  for (let i = 0; i < bin2.length; i++) {
+    const j = bin2.length - i - 1;
+    n2 += bin2[j] * 2 ** i;
+  }
+
+  num = n1 + n2;
+
+  for (let i = 0; i < num; i++) {
+    if (2 ** i <= num) {
+      maxNum = i;
+    }
+  }
+
+  for (let i = 0; i <= maxNum; i++) {
+    const j = maxNum - i;
+
+    if (2 ** j <= num) {
+      count++;
+      arr.push(count);
+      num -= 2 ** j;
+    } else if (2 ** j > num) {
+      arr.push(count);
+    }
+
+    count = 0;
+  }
+
+  return arr.join("");
 }
 
 console.log(solution("10", "11")); // "101"
