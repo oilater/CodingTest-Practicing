0cf31dcb09eef1a4eef9222e835ec7c83acbdeb3
김성현 <oilater@naver.com>
2023-04-18 09:38:44 +0900

    solved

--- 2023-04-17/문자열계산하기/solution1.js
+++ 2023-04-17/문자열계산하기/solution1.js
@@ -1,8 +1,20 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120902?language=javascript
 
 function solution(my_string) {
-  var answer = 0;
-  return answer;
+  const arr = my_string.split(" ");
+  let calc = Number(arr[0]);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "+") {
+      calc += Number(arr[i + 1]);
+    }
+
+    if (arr[i] === "-") {
+      calc -= Number(arr[i + 1]);
+    }
+  }
+
+  return calc;
 }
 
 console.log(solution("3 + 4")); // 7
