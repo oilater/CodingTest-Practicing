b52e4305e025ff3e78fda5d3ff905a574424b315
김성현 <oilater@naver.com>
2023-04-21 16:57:41 +0900

    chicken shivar

--- 2023-04-20/치킨쿠폰/solution1.js
+++ 2023-04-20/치킨쿠폰/solution1.js
@@ -1,10 +1,22 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120884?language=javascript
 
 function solution(chicken) {
-  const answer = 0;
+  let answer = 0;
+  let q = chicken;
+  let r = 0;
+
+  while (q >= 10) {
+    r = q % 10;
+    q = Math.trunc(q / 10);
+
+    answer += q;
+    q += r;
+  }
 
   return answer;
 }
-
 console.log(solution(100)); // 11
 console.log(solution(1081)); //	120
+console.log(solution(1999)); //	222
+
+// 다시 풀어보기 후우.. ㅠㅋ
