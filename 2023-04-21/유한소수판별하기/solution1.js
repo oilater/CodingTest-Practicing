d82dd4faaeff5f45ed648ec830070d23d9851816
김성현 <oilater@naver.com>
2023-04-22 00:22:33 +0900

    commit today

---
+++ 2023-04-21/유한소수판별하기/solution1.js
@@ -0,0 +1,28 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120878?language=javascript
+
+function solution(a, b) {
+  let share = 0;
+  let mom = 0;
+  for (let i = 1; i < b; i++) {
+    if (a % i === 0 && b % i === 0) {
+      share = i;
+    }
+  }
+  mom = b / share;
+
+  while (mom !== 1) {
+    if (mom % 2 === 0) {
+      mom /= 2;
+    } else if (mom % 5 === 0) {
+      mom /= 5;
+    } else {
+      return 2;
+    }
+  }
+
+  return 1;
+}
+
+console.log(solution(7, 20)); // 1
+console.log(solution(11, 22)); // 1
+console.log(solution(12, 21)); // 2
