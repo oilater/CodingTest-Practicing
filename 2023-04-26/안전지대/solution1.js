436bf168bcc30f06c50d4a0d66de81a78c064316
김성현 <oilater@naver.com>
2023-04-27 22:05:12 +0900

    so hard

---
+++ 2023-04-26/안전지대/solution1.js
@@ -0,0 +1,118 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120866?language=javascript
+
+function solution(board) {
+  let answer = 0;
+  const entire = board.length ** 2;
+  let danger = 0;
+
+  // 각 모서리에 지뢰가 있을 때 설정
+  if (board[0][0] === 1) {
+    board[0][0] = 1;
+    board[0][1] = 1;
+    board[1][0] = 1;
+    board[1][1] = 1;
+  }
+
+  if (board[0][board.length - 1] === 1) {
+    board[0][board.length - 2] = 1;
+    board[1][board.length - 2] = 1;
+    board[1][board.length - 1] = 1;
+  }
+
+  if (board[board.length - 1][0] === 1) {
+    board[board.length - 2][0] = 1;
+    board[board.length - 2][1] = 1;
+    board[board.length - 1][1] = 1;
+  }
+
+  if (board[board.length - 1][board.length - 1]) {
+    board[board.length - 2][board.length - 2] = 1;
+    board[board.length - 2][board.length - 1] = 1;
+    board[board.length - 1][board.length - 2] = 1;
+  }
+
+  // 변 설정
+  for (let i = 1; i < board.length - 1; i++) {
+    if (board[0][i] === 1) {
+      board[0][i - 1] = 1;
+      board[0][i + 1] = 1;
+      board[1][i - 1] = 1;
+      board[1][i] = 1;
+      board[1][i + 1] = 1;
+      break;
+    }
+
+    if (board[i][0] === 1) {
+      board[i - 1][0] = 1;
+      board[i - 1][1] = 1;
+      board[i][1] = 1;
+      board[i + 1][0] = 1;
+      board[i + 1][1] = 1;
+      break;
+    }
+
+    if (board[board.length - 1][i] === 1) {
+      board[board.length - 1][i - 1] = 1;
+      board[board.length - 1][i + 1] = 1;
+      board[board.length - 2][i - 1] = 1;
+      board[board.length - 2][i] = 1;
+      board[board.length - 2][i + 1] = 1;
+      break;
+    }
+
+    if (board[i][board.length - 1] === 1) {
+      board[i - 1][board.length - 2] = 1;
+      board[i - 1][board.length - 1] = 1;
+      board[i][board.length - 2] = 1;
+      board[i + 1][board.length - 2] = 1;
+      board[i + 1][board.length - 1] = 1;
+      break;
+    }
+  }
+
+  for (let i = 1; i < board.length - 1; i++) {
+    for (let j = 1; j < board.length - 1; j++) {
+      if (board[i][j] === 1) {
+        board[i - 1][j - 1] = 1;
+        board[i - 1][j] = 1;
+        board[i - 1][j + 1] = 1;
+        board[i][j - 1] = 1;
+        board[i][j + 1] = 1;
+        board[i + 1][j - 1] = 1;
+        board[i + 1][j] = 1;
+        board[i + 1][j + 1] = 1;
+        break;
+      }
+    }
+  }
+  return board;
+}
+
+console.log(
+  solution([
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+  ])
+); // 16
+// console.log(
+//   solution([
+//     [0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0],
+//     [0, 0, 1, 1, 0],
+//     [0, 0, 0, 0, 0],
+//   ])
+// ); // 13
+// console.log(
+//   solution([
+//     [1, 1, 1, 1, 1, 1],
+//     [1, 1, 1, 1, 1, 1],
+//     [1, 1, 1, 1, 1, 1],
+//     [1, 1, 1, 1, 1, 1],
+//     [1, 1, 1, 1, 1, 1],
+//     [1, 1, 1, 1, 1, 1],
+//   ])
+// ); // 0
