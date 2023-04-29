6b6365ed3c1c09a3d16f897485934d6f37ff78ca
김성현 <oilater@naver.com>
2023-04-29 23:05:54 +0900

    push

--- 2023-04-29/옹알이(1)/solution1.js
+++ 2023-04-29/옹알이(1)/solution1.js
@@ -1,7 +1,38 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120956?language=javascript
 
 function solution(babbling) {
-  var answer = 0;
+  let answer = 0;
+  let num = 0;
+  let s = "";
+  const ong = ["aya", "ye", "woo", "ma"];
+
+  for (let i = 0; i < babbling.length; i++) {
+    for (let j = 0; j < babbling[i].length; j++) {
+      s += babbling[i][j];
+
+      if (ong.includes(s)) {
+        num++;
+        s = "";
+      }
+    }
+
+    if (num >= 1) {
+      answer++;
+      num = 0;
+    }
+
+    if (s !== "") {
+      if (s === babbling[i]) {
+        s = "";
+        continue;
+      }
+
+      if (s.length < babbling[i].length) {
+        answer--;
+        s = "";
+      }
+    }
+  }
 
   return answer;
 }
