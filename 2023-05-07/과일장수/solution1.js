b3e35ea3e090048cc596a1ad2a927356547cade6
김성현 <oilater@naver.com>
2023-05-07 08:39:19 +0900

    hihi

--- 2023-05-07/과일장수/solution1.js
+++ 2023-05-07/과일장수/solution1.js
@@ -1,7 +1,14 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/135808?language=javascript
 
 function solution(k, m, score) {
+  score = score.sort((a, b) => b - a);
   let answer = 0;
+
+  for (let i = 1; i <= score.length; i++) {
+    if (score[m * i - 1] !== undefined) {
+      answer += score[m * i - 1] * m;
+    }
+  }
 
   return answer;
 }
---
+++ 2023-05-07/카드뭉치/solution1.js
@@ -0,0 +1,22 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=javascript
+
+function solution(cards1, cards2, goal) {
+  let answer = "";
+  
+  return answer;
+}
+
+console.log(
+  solution(
+    ["i", "drink", "water"],
+    ["want", "to"],
+    ["i", "want", "to", "drink", "water"]
+  )
+); //"Yes"
+console.log(
+  solution(
+    ["i", "water", "drink"],
+    ["want", "to"],
+    ["i", "want", "to", "drink", "water"]
+  )
+); //"No"
